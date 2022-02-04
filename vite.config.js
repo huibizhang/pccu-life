6695b70c9b93359fb11d47ef16de872ab8edd7c3
搋兔 <naruto6404@hotmail.com>
2022-02-05 03:34:37 +0800

    update: pwa setting

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,35 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({}),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'PCCU Life',
+        short_name: 'PCCU Life',
+        // display: "fullscreen",
+        // start_url: "/index.html",
+        description: 'PCCU 生活資訊',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      },
+    }),
   ]
 })
