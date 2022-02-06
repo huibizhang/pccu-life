efd9fa58dccb27124aefb1b55adae132c73b60d0
搋兔 <naruto6404@hotmail.com>
2022-02-07 04:52:37 +0800

    fix: setting of VitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.png", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: 'PCCU Life',
-        short_name: 'PCCU Life',
+        name: "PCCU Life",
+        short_name: "PCCU Life",
         // display: "fullscreen",
         // start_url: "/index.html",
-        description: 'PCCU 生活資訊',
-        theme_color: '#000000',
+        description: "PCCU 生活資訊",
+        theme_color: "#000000",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ]
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
     }),
-  ]
-})
+  ],
+});
